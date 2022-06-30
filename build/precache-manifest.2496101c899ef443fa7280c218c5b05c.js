95a495e0c91d84453e082046e9535ec9514022b0
Atulya Vishwanath <atulyav97@gmail.com>
2022-06-29 19:30:34 -0700

    Modifying files to work with pipeline

---
+++ build/precache-manifest.2496101c899ef443fa7280c218c5b05c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "274366c04036d650f89c",
+    "url": "/static/js/main.274366c0.chunk.js"
+  },
+  {
+    "revision": "cc6a901fc5d09c0efd26",
+    "url": "/static/js/1.cc6a901f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "846aa7c9721b2a36bf56e621f0608c7d",
+    "url": "/index.html"
+  }
+];
